4c9bf59f58fa6c6bed416802fe5396a63079f001
29-29 <ackx29sodo@gmail.com>
2025-06-25 10:58:03 +0800

    use of icons

--- FRONTEND/app.vue
+++ FRONTEND/app.vue
@@ -1,8 +1,36 @@
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui';
+const items = [
+  {
+    label: 'Home',
+    icon: 'i-lucide-house',
+    to: '/'
+  },
+  {
+    label: 'Shop',
+    icon: 'i-lucide-store',
+    to: '/shop'
+  },
+  {
+    label: 'About',
+    icon: 'i-lucide-circle-alert',
+    to: '/about'
+  },
+  {
+    label: 'Contact',
+    icon: 'i-lucide-phone',
+    to: '/contact'
+  }
+] satisfies NavigationMenuItem[]
+</script>
+
 <template>
   <div>
-    <NuxtLayout>
-      <Navbar />
-      <NuxtPage class="m-2" />
-    </NuxtLayout>
+    <div class="px-4 flex flex-row items-center gap-2">
+      <NuxtLink to="/" class="text-xl">Shop</NuxtLink>
+      <UNavigationMenu :items="items" :highlight="true" class="w-full border-gray-600" />
+    </div>
+    <USeparator />
+    <NuxtPage class="m-2" />
   </div>
 </template>
