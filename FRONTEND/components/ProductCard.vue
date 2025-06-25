6444aad7b4a6c2ae50e0681172fab39c579999e2
29-29 <ackx29sodo@gmail.com>
2025-06-25 13:32:26 +0800

    added view and add to cart buttons

--- FRONTEND/components/ProductCard.vue
+++ FRONTEND/components/ProductCard.vue
@@ -7,16 +7,20 @@ const props = defineProps<{
 </script>
 
 <template>
-	<UCard variant="solid" class="flex flex-col justify-between">
+	<UCard variant="solid" class="flex flex-col">
 	<!-- <div class="p-3 w-full h-full inline-block border border-gray-200 shadow-lg flex flex-col flex-grow justify-between"> -->
 		<img :src="props.item.images[0]" class="aspect-square w-full object-contain" />
 		<div class="px-1 py-1">
 			<h3 class="truncate text-lg font-semibold">{{ props.item.title }}</h3>
-			<div class="flex items-center gap-2">
+			<div class="flex items-center gap-2" style="width: 50%;">
 				<UProgress v-model="props.item.rating" :max="5" />
 				<span class="text-primary">{{ props.item.rating }}</span>
 			</div>
 			<p class="text-green-700 font-bold">${{ props.item.price }}</p>
+		</div>
+		<div class="grid grid-flow-col text-center gap-2">
+			<UButton block label="View" variant="outline" :to="`/shop/${props.item.id}`" />
+			<UButton block label="Add to Cart" icon="i-lucide-plus" />
 		</div>
 	</UCard>
 	<!-- </div> -->
