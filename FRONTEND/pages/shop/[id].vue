85a6bb9eee4b94677eb52bb77f287e928b87f9bd
29-29 <ackx29sodo@gmail.com>
2025-06-26 18:27:56 +0800

    breadcrumb and back button on product details page

--- FRONTEND/pages/shop/[id].vue
+++ FRONTEND/pages/shop/[id].vue
@@ -1,15 +1,36 @@
 <script setup lang="ts">
+import { capitalize } from 'vue';
 import type { Item } from '~/models/Item';
+// import { BreadcrumbItem } from '@nuxt/ui';
 
 const route = useRoute();
 const { data, error, pending } = await useProduct(Number(route.params.id))
 const item = computed(() => data.value as Item | null)
 let quantity = ref(1)
+
+const breadcrumbs = ref<BreadcrumbItem[]>([
+	{
+		icon: "i-lucide-shopping-cart",
+		label: "Shop",
+		to: "/shop"
+	},
+	{
+		label: capitalize(item.value?.category)
+		// to:
+	},
+	{
+		label: item.value?.title
+	}
+])
+
 </script>
 
 <template>
-	<div class="flex flex-col p-5 gap-5 max-w-screen-lg mx-auto">
-		<span>breadcrumb</span>
+	<div class="flex flex-col py-2 px-5 gap-5 max-w-screen-lg mx-auto">
+		<div class="flex flex-row items-center gap-2">
+			<UButton icon="i-lucide-arrow-left" variant="ghost" class="cursor-pointer" @click="$router.back" />
+			<UBreadcrumb :items="breadcrumbs" />
+		</div>
 		<USeparator />
 		<div class="flex flex-row gap-5 mb-5">
 			<UCarousel dots v-slot="{ item }" :items="item?.images" class="border rounded-lg max-w-lg">
@@ -18,7 +39,7 @@ let quantity = ref(1)
 			<div class="w-full flex flex-col gap-3" style="min-width: 50%;">
 				<h1 class="text-2xl font-semibold">{{ item?.title }}</h1>
 				<div class="flex flex-row items-center gap-2 text-secondary max-w-xs" style="width: 50%;">
-					<UProgress v-model="item.rating" :max="5" color="secondary" />
+					<UProgress v-model="data.rating" :max="5" color="secondary" />
 					<span>{{ data?.rating }}</span>
 					<UIcon name="i-lucide-star" class="size-6" />
 				</div>
