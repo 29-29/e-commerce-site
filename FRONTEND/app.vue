068b5463cc921be8d5ffb93a9823a30fb022a0da
29-29 <ackx29sodo@gmail.com>
2025-06-26 18:11:04 +0800

    updated cart and account navigation

--- FRONTEND/app.vue
+++ FRONTEND/app.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { UNavigationMenu } from '#components';
 import type { NavigationMenuItem } from '@nuxt/ui';
 
 const items = [
@@ -23,13 +24,35 @@ const items = [
     to: '/contact'
   }
 ] satisfies NavigationMenuItem[]
+
+const accountItems = [
+  {
+    label: "Profile",
+    icon: "i-lucide-user"
+  },
+  {
+    label: "Log Out",
+    icon: "i-lucide-log-out"
+  }
+] satisfies NavigationMenuItem[]
 </script>
 
 <template>
   <div>
-    <div class="px-4 flex flex-row items-center gap-2">
-      <NuxtLink to="/" class="text-xl">Shop</NuxtLink>
-      <UNavigationMenu :items="items" :highlight="true" class="w-full border-gray-600" />
+    <div class="px-4 flex flex-row items-center justify-between">
+      <div class="flex flex-row items-center gap-2">
+        <NuxtLink to="/" class="text-xl">Shop</NuxtLink>
+        <UNavigationMenu :items="items" :highlight="true" class="w-full border-gray-600" />
+      </div>
+      <div class="flex flex-row gap-2">
+        <UButton label="Cart" icon="i-lucide-shopping-cart" variant="ghost" />
+        <UPopover>
+          <UButton icon="i-lucide-circle-user-round" variant="ghost" />
+          <template #content>
+            <UNavigationMenu :items="accountItems" orientation="vertical" class="p-1" />
+          </template>
+        </UPopover>
+      </div>
     </div>
     <USeparator />
     <NuxtPage class="m-2" />
