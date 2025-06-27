5f8cd0fbf5783e9f18e52f8bcef82700cd33c79e
29-29 <ackx29sodo@gmail.com>
2025-06-27 16:27:47 +0800

    cart page

---
+++ FRONTEND/pages/cart.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { CartBooking } from '~/models/CartBooking';
+import type { CartItem } from '~/models/CartItem';
+
+const cart = [
+	{
+		cartBookingId: 1,
+		itemId: 2,
+		userId: 1,
+		quantity: 2
+	}
+] satisfies CartBooking[]
+</script>
+
+<template>
+	<div class="flex flex-col gap-3 p-3">
+		<div class="flex gap-2 items-center">
+			<h1 class="text-3xl font-bold">Your Cart</h1>
+			<UBadge label="1" size="xl" class="font-semibold rounded-full" variant="soft" />
+		</div>
+		<USeparator />
+		<CartItem v-for="cartItem in cart" :item="cartItem" />
+		<div>
+		</div>
+	</div>
+</template>
